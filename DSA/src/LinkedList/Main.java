ec933b653ec8df4def35e815b5a000f1f7db4282
HuyLe2602 <yang260204@gmail.com>
2026-05-04 19:59:41 +0700

    em gui anh bai 4/5

---
+++ DSA/src/LinkedList/Main.java
@@ -0,0 +1,11 @@
+﻿package LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(10);
+        linkedList.printAll();
+        linkedList.printList();
+
+    }
+}
