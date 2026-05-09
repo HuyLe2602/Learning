1a281de6c2250a880a501760ef70a5158c7fb29d
HuyLe2602 <yang260204@gmail.com>
2026-05-09 20:16:56 +0700

    em gui bai 9/5

--- DSA/src/LinkedList/LinkedList.java
+++ DSA/src/LinkedList/LinkedList.java
@@ -114,7 +114,8 @@ public class LinkedList {
         }
         return temp;
     }
-    public boolean set(int index,int value) {
+
+    public boolean set(int index, int value) {
         Node temp = get(index);
         if (temp != null) {
             temp.value = value;
@@ -122,31 +123,33 @@ public class LinkedList {
         }
         return false;
     }
-    public boolean insert(int index, int value){
-        if(index <0 || index > length)return false;
 
-        if (index ==0){
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+
+        if (index == 0) {
             prepend(value);
             return true;
         }
-        if (index == length){
+        if (index == length) {
             append(value);
             return true;
         }
-        Node newNode= new Node(value);
+        Node newNode = new Node(value);
 
-        Node temp = get(index-1);
+        Node temp = get(index - 1);
         newNode.next = temp.next;
         temp.next = newNode;
         length++;
         return true;
     }
-    public Node remove(int index){
-        if (index <0 || index >= length)return null;
-        if (index ==0) return removeFirst();
-        if (index == length-1) return removeLast();
 
-        Node pre = get(index-1);
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+
+        Node pre = get(index - 1);
         Node temp = pre.next;
 
         pre.next = temp.next;
@@ -156,7 +159,7 @@ public class LinkedList {
 
     }
 
-    public void reverse(){
+    public void reverse() {
         Node temp = head;
         head = tail;
         tail = temp;
@@ -164,7 +167,7 @@ public class LinkedList {
         Node after = temp.next;
         Node before = null;
 
-        for (int i=0; i<length; i++){
+        for (int i = 0; i < length; i++) {
             after = temp.next;
             temp.next = before;
             before = temp;
@@ -196,35 +199,50 @@ public class LinkedList {
     }
 
     //Lưu ý phải chỉnh lại append để phù hợp với hàm này
-    public void removeDuplicates(){
+    public void removeDuplicates() {
         Set<Integer> set = new LinkedHashSet<>();
         Node current = head;
-        while(current != null){
+        while (current != null) {
             set.add(current.value);
             current = current.next;
         }
         makeEmpty();
-        for(Integer value: set){
+        for (Integer value : set) {
             append(value);
         }
         length = set.size();
     }
-
+    //Code moi ngay 8/5
     public int binaryToDecimal() {
         int i = 0;
-        while(head != null) {
+        while (head != null) {
             i = i * 2 + head.value;
             head = head.next;
         }
         return i;
     }
 
+    //Code moi ngay 9/5
+    public void partitionList(int x) {
+        if (head == null) {
+            return;
+        }
+        Node s1 = new Node(-1);
+        Node s2 = new Node(-2);
+        Node newHead = s1;
+        Node newTail = s2;
+        while (head != null) {
+            if (head.value < x) {
+                s1.next = head;
+                s1 = s1.next;
+            } else {
+                s2.next = head;
+                s2 = s2.next;
+            }
+            head = head.next;
+        }
 
 
-
-
-
-
-
+    }
 
 }
