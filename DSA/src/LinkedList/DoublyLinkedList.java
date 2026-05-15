2bd730d1dea773465db36285a09765962818e88d
HuyLe2602 <yang260204@gmail.com>
2026-05-15 19:16:41 +0700

    em gui anh bai15/5

--- DSA/src/LinkedList/DoublyLinkedList.java
+++ DSA/src/LinkedList/DoublyLinkedList.java
@@ -76,7 +76,96 @@ public class DoublyLinkedList {
             length--;
             return temp;
         }
+        //Code mới ngày 15/5
+        public void prepend(int value) {
+            Node newNode = new Node(value);
+            if (length == 0) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode.next = head; //newNode.next sẽ là head
+                head.prev = newNode; //head.prev sẽ là newNode
+                head = newNode; //head sẽ là node mới
+            }
+            length++;   // tăng độ dài của danh sách lên 1
+        }
 
+         public Node removeFirst() {
+            if (length == 0) return null;
+            Node temp = head;
+            if (length == 1) {
+                head = null;
+                tail = null;
+            } else {
+                head = head.next; //head sẽ là node tiếp theo
+                head.prev = null; //head.prev sẽ là null
+                temp.next = null; //temp.next sẽ là null
+            }
+            length--;   // giảm độ dài của danh sách xuống 1
+            return temp;
+        }
+
+        public Node get(int index) {
+            if (index < 0 || index >= length) return null;
+            Node temp = head;
+            if (index < length / 2) {
+                for (int i = 0; i < index; i++) {
+                    temp = temp.next;
+                }       // nếu node nằm ở nữa đầu danh sach thì ta sẽ duyệt từ head đến node đó
+            } else {
+                temp = tail;
+                for (int i = length - 1; i > index; i--) {
+                    temp = temp.prev;
+                }
+            }     // nếu node nằm ở nữa cuối danh sach thì ta sẽ duyệt từ tail đến node đó
+            return temp;
+        }
+
+        public boolean set(int index, int value) {
+            Node temp = get(index);
+            if (temp != null) {
+                temp.value = value;
+                return true;
+            }
+            return false;
+        }
+
+         public boolean insert(int index, int value) {
+            if (index < 0 || index > length) return false;
+            if (index == 0) {
+                prepend(value);
+                return true;
+            }
+            if (index == length) {
+                append(value);
+                return true;
+            }
+            Node newNode = new Node(value);
+            Node before = get(index - 1);
+            Node after = before.next;
+
+            before.next = newNode; //before.next sẽ là newNode
+            newNode.prev = before; //newNode.prev sẽ là before
+            newNode.next = after; //newNode.next sẽ là after
+            after.prev = newNode; //after.prev sẽ là newNode
+            length++;
+            return true;
+        }
+
+         public Node remove(int index) {
+             if (index < 0 || index >= length) return null;
+             if (index == 0) return removeFirst();
+             if (index == length - 1) return removeLast();
+
+             Node temp = get(index);
+
+             temp.prev.next = temp.next; //temp.prev.next sẽ là temp.next
+             temp.next.prev = temp.prev; //temp.next.prev sẽ là temp.prev
+             temp.next = null; //temp.next sẽ là null
+             temp.prev = null; //temp.prev sẽ là null
+             length--;
+             return temp;
+         }
 
 
 }
