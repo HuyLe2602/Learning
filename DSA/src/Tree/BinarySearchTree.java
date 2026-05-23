105bdd897ce5c2bffa5c87216f52cc7396889d43
HuyLe2602 <yang260204@gmail.com>
2026-05-23 23:06:39 +0700

    em gui anh bai 23/5

---
+++ DSA/src/Tree/BinarySearchTree.java
@@ -0,0 +1,48 @@
+﻿package Tree;
+
+public class BinarySearchTree {
+    private Node root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int value) {
+        Node newNode = new Node(value);
+
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        Node temp = root;
+
+        while (true) {
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    public void printInOrder(Node node) {
+        if (node != null) {
+            printInOrder(node.left);
+            System.out.println(node.value);
+            printInOrder(node.right);
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    } 
+}
---
+++ DSA/src/Tree/Node.java
@@ -0,0 +1,11 @@
+﻿package Tree;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        this.value = value;
+    }
+}
