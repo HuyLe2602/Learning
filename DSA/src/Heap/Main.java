547fa2690c01da4edb360485410021f1e1e93079
HuyLe2602 <yang260204@gmail.com>
2026-05-30 20:19:53 +0700

    em gui anh bai 29+30/5

---
+++ DSA/src/Heap/Main.java
@@ -0,0 +1,22 @@
+﻿package Heap;
+
+public class Main {
+    static void main() {
+        Heap myheap = new Heap(11);
+        myheap.insert(1);
+        myheap.insert(2);
+        myheap.insert(3);
+        myheap.insert(4);
+        myheap.insert(5);
+        myheap.insert(6);
+        myheap.insert(7);
+        myheap.insert(8);
+        myheap.insert(9);
+
+        System.out.println(myheap.getHeap());
+
+        myheap.insert(10);
+
+        System.out.println(myheap.getHeap());
+    }
+}
