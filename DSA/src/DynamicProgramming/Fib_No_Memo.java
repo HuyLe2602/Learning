02c898355646b05a6b0f52ea7fdccf8c913e203d
HuyLe2602 <yang260204@gmail.com>
2026-06-07 20:05:58 +0700

    em gui bai 7/6

---
+++ DSA/src/DynamicProgramming/Fib_No_Memo.java
@@ -0,0 +1,18 @@
+﻿package DynamicProgramming;
+
+public class Fib_No_Memo {
+    static int counter =0;
+    public static int fib(int n) {
+        counter++;
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    static void main() {
+        int n = 10;
+        System.out.println("Fibonacci of " + n + " is: " + fib(n));
+        System.out.println("Number of calls: " + counter);
+    }
+}
---
+++ DSA/src/DynamicProgramming/Fib_With_Memo.java
@@ -0,0 +1,24 @@
+﻿package DynamicProgramming;
+
+public class Fib_With_Memo {
+    static Integer[] memo= new Integer[100];
+    static int counter = 0;
+
+    public static int fib(int n) {
+        counter++;
+        if (memo[n] <= 1) {
+            return memo[n];
+        }
+        if (n== 0|| n==1) {
+            return n;
+        }
+        memo[n] = fib(n - 1) + fib(n - 2);
+        return memo[n];
+    }
+
+    static void main() {
+        int n = 10;
+        System.out.println("Fibonacci of " + n + " is: " + fib(n));
+        System.out.println("Number of calls: " + counter);
+    }
+}
